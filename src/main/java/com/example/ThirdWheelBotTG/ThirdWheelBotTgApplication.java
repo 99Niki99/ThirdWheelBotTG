a3cb08c9dbd1d764f26bee57d9b7acdc943018d5
Nikita <ltkmabyf1979@GMAIL.COM>
2023-08-04 16:48:08 -0500

    First Commit

---
+++ src/main/java/com/example/ThirdWheelBotTG/ThirdWheelBotTgApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThirdWheelBotTG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdWheelBotTgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdWheelBotTgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ThirdWheelBotTG/ThirdWheelBotTgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThirdWheelBotTG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdWheelBotTgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
